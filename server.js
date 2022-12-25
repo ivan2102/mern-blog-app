5c8cc3f4f33d11f987df2176a7315ea371d170b8
Ivan <radisav80@yahoo.com>
2022-12-25 23:17:09 +0100

    new changes new port added

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(notFoundError)
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5002
 app.listen(PORT, console.log(`Server is running on port ${PORT}`))
 
 
